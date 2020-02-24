273991f112380de2a308e21144a3dc2f85dd59f0
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-24 20:05:00 +0530

    moved ticTacToeGame local variable as a field variable and initialized it in setUp method

--- src/test/java/com/kata/tictactoe/TicTacToeGameTest.java
+++ src/test/java/com/kata/tictactoe/TicTacToeGameTest.java
@@ -1,5 +1,6 @@
 package com.kata.tictactoe;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -8,16 +9,20 @@ import static org.junit.Assert.assertThat;
 public class TicTacToeGameTest {
 
     private static final String EMPTY_STRING = "";
+    private TicTacToeGame ticTacToeGame;
+
+    @Before
+    public void setUp() {
+        ticTacToeGame = new TicTacToeGame();
+    }
 
     @Test
     public void ticTacToeGameShouldReturn_EmptyString_IfPlayer_X_MovesOn_0_0_Position() {
-        TicTacToeGame ticTacToeGame = new TicTacToeGame();
         assertThat(ticTacToeGame.playGame(0,0,'X'), is(EMPTY_STRING));
     }
 
     @Test
     public void ticTacToeGameShouldReturn_EmptyString_IfPlayerXGoes1stAt_0_0_And_Player_O_Goes2ndAt_0_2_Position() {
-        TicTacToeGame ticTacToeGame = new TicTacToeGame();
         assertThat(ticTacToeGame.playGame(0,0,'X'), is(EMPTY_STRING));
         assertThat(ticTacToeGame.playGame(0,2,'0'), is(EMPTY_STRING));
     }
