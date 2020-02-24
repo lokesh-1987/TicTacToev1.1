f2ef964f4c0627636b92844093433e642907faea
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-25 00:15:47 +0530

    Moved position local variable to field variable

--- src/test/java/com/kata/tictactoe/PositionTest.java
+++ src/test/java/com/kata/tictactoe/PositionTest.java
@@ -7,21 +7,20 @@ import static org.junit.Assert.assertThat;
 
 public class PositionTest {
 
+    Position position = Position.ONE;
+
     @Test
     public void positionReturnsRowAs0IfInitializedWithONE() {
-        Position position = Position.ONE;
         assertThat(position.getRow(), is(0));
     }
 
     @Test
     public void positionReturnsColumnAs0IfInitializedWithONE() {
-        Position position = Position.ONE;
         assertThat(position.getColumn(), is(0));
     }
 
     @Test
     public void positionReturnsONEIfInitializedWith1() {
-        Position position = Position.ONE;
         Position pos = position.getPosition(1);
         assertThat(pos.name(), is(Position.ONE.name()));
     }
