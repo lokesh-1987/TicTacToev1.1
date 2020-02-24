1aacdd6c1f16223de673d1c56bffa08859ad7e74
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-24 19:58:58 +0530

    Added a test to return empty string for 1st and 2nd move by the player X and O alternatively

--- src/test/java/com/kata/tictactoe/TicTacToeGameTest.java
+++ src/test/java/com/kata/tictactoe/TicTacToeGameTest.java
@@ -12,4 +12,11 @@ public class TicTacToeGameTest {
         TicTacToeGame ticTacToeGame = new TicTacToeGame();
         assertThat(ticTacToeGame.playGame(0,0,'X'), is(""));
     }
+
+    @Test
+    public void ticTacToeGameShouldReturn_EmptyString_IfPlayerXGoes1stAt_0_0_And_Player_O_Goes2ndAt_0_2_Position() {
+        TicTacToeGame ticTacToeGame = new TicTacToeGame();
+        assertThat(ticTacToeGame.playGame(0,0,'X'), is(""));
+        assertThat(ticTacToeGame.playGame(0,2,'0'), is(""));
+    }
 }
