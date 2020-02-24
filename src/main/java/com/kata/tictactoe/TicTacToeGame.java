6cf848117d1493b5be0a7eb4b16a806442f4d04b
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-24 22:24:11 +0530

    Extracted constant for Player and Won String

--- src/main/java/com/kata/tictactoe/TicTacToeGame.java
+++ src/main/java/com/kata/tictactoe/TicTacToeGame.java
@@ -5,6 +5,8 @@ class TicTacToeGame {
     private static final String EMPTY_STRING = "";
     private static final String GAME_IS_DRAW = "Game is Draw!";
     private static final String POSITION_IS_INVALID = "Position is Invalid!";
+    private static final String PLAYER = "Player ";
+    private static final String WON = " Won!";
     private TicTacToeBoard ticTacToeBoard;
 
     TicTacToeGame(TicTacToeBoard ticTacToeBoard) {
@@ -19,7 +21,7 @@ class TicTacToeGame {
         if (ticTacToeBoard.isBoardFull()) {
             return GAME_IS_DRAW;
         } else if (ticTacToeBoard.isFirstRowFullOfEitherXOr0(value)) {
-            return "Player "+value+ " Won!";
+            return PLAYER +value+ WON;
         }
         return EMPTY_STRING;
     }
