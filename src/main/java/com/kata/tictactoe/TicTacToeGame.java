8f4a6d1d4e8a4afd419bf4ea14db6bc46e6e70f7
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-24 20:28:14 +0530

    extracted constant for GAME_IS_DRAW

--- src/main/java/com/kata/tictactoe/TicTacToeGame.java
+++ src/main/java/com/kata/tictactoe/TicTacToeGame.java
@@ -3,6 +3,7 @@ package com.kata.tictactoe;
 class TicTacToeGame {
 
     private static final String EMPTY_STRING = "";
+    private static final String GAME_IS_DRAW = "Game is Draw!";
     private TicTacToeBoard ticTacToeBoard;
 
     TicTacToeGame(TicTacToeBoard ticTacToeBoard) {
@@ -12,7 +13,7 @@ class TicTacToeGame {
     String playGame(int row, int col, char value) {
         ticTacToeBoard.setValue(row,col,value);
         if (ticTacToeBoard.isBoardFull()) {
-            return "Game is Draw!";
+            return GAME_IS_DRAW;
         }
         return EMPTY_STRING;
     }
