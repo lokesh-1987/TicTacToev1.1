6e3b64f404c1816dab54e5fc996cdd74710da480
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-24 19:59:52 +0530

    extracted constant for empty string in test class

--- src/test/java/com/kata/tictactoe/TicTacToeGameTest.java
+++ src/test/java/com/kata/tictactoe/TicTacToeGameTest.java
@@ -7,16 +7,18 @@ import static org.junit.Assert.assertThat;
 
 public class TicTacToeGameTest {
 
+    private static final String EMPTY_STRING = "";
+
     @Test
     public void ticTacToeGameShouldReturn_EmptyString_IfPlayer_X_MovesOn_0_0_Position() {
         TicTacToeGame ticTacToeGame = new TicTacToeGame();
-        assertThat(ticTacToeGame.playGame(0,0,'X'), is(""));
+        assertThat(ticTacToeGame.playGame(0,0,'X'), is(EMPTY_STRING));
     }
 
     @Test
     public void ticTacToeGameShouldReturn_EmptyString_IfPlayerXGoes1stAt_0_0_And_Player_O_Goes2ndAt_0_2_Position() {
         TicTacToeGame ticTacToeGame = new TicTacToeGame();
-        assertThat(ticTacToeGame.playGame(0,0,'X'), is(""));
-        assertThat(ticTacToeGame.playGame(0,2,'0'), is(""));
+        assertThat(ticTacToeGame.playGame(0,0,'X'), is(EMPTY_STRING));
+        assertThat(ticTacToeGame.playGame(0,2,'0'), is(EMPTY_STRING));
     }
 }
